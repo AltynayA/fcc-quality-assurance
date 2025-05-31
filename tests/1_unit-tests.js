8339da7280998530eb03983d1fb68418de5b22e7
altynay <goldmoon321123@gmail.com>
2025-05-31 16:19:03 +0500

    #5 and #6

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -33,17 +33,17 @@ suite('Unit Tests', function () {
   suite('Equality', function () {
     // #5
     test('#equal, #notEqual', function () {
-      assert.fail(12, '12', 'Numbers are coerced into strings with ==');
-      assert.fail({ value: 1 }, { value: 1 }, '== compares object references');
-      assert.fail(6 * '2', '12');
-      assert.fail(6 + '2', '12');
+      assert.Equal(12, '12', 'Numbers are coerced into strings with ==');
+      assert.NotEqual({ value: 1 }, { value: 1 }, '== compares object references');
+      assert.Equal(6 * '2', '12');
+      assert.NotEqual(6 + '2', '12');
     });
     // #6
     test('#strictEqual, #notStrictEqual', function () {
-      assert.fail(6, '6');
-      assert.fail(6, 3 * 2);
-      assert.fail(6 * '2', 12);
-      assert.fail([1, 'a', {}], [1, 'a', {}]);
+      assert.notStrictEqual(6, '6');
+      assert.strictEqual(6, 3 * 2);
+      assert.strictEqual(6 * '2', 12);
+      assert.notStrictEqual([1, 'a', {}], [1, 'a', {}]);
     });
     // #7
     test('#deepEqual, #notDeepEqual', function () {
